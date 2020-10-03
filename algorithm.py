a7457c21bc535e819b713396467b1908c9b9eb5f
theg3ntlem4n <ar3x.k1m@gmail.com>
2020-10-03 02:00:06 -0500

    enhanced image recognition algorithm

--- algorithm.py
+++ algorithm.py
@@ -8,21 +8,26 @@ text = pytesseract.image_to_string(Image.open('Receipt2.JPG'))
 #text = spell(text)
 sorted_text = text.lower().split()
 
-total = ""
+total = " "
+
+f = open("total.json", "w")
 
 for x in sorted_text:
     if x == 'total' or x == 'total:':
         total = sorted_text[sorted_text.index(x) + 1]
+        break
     elif x == 'charge' or x == 'charge:':
         total = sorted_text[sorted_text.index(x) + 1]
+        break
     elif x == 'subtotal' or x == 'subtotal:':
         for y in sorted_text:
             if y == 'tax' or y == 'tax':
                 total = int(sorted_text[sorted_text.index(x) + 1]) + int(sorted_text[sorted_text.index(y) + 1])
+                break
     else:
         total = "Price not found, please retake picture"
 
 print(total)
-print(text)
+f.write("{total: " + total + "}\n")
 
         
